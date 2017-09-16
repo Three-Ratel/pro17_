53e3dc4d018cbc927e8ba27f45a0b92c8b11cb94
ZQOFFICE0\wuxingpu <windfishing5@gmail.com>
2017-09-16 18:57:50 +0800

    s18

--- page_demo/app01/views.py
+++ page_demo/app01/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render,HttpResponse
-from app01.models import  *
+from django.shortcuts import render, HttpResponse
+from app01.models import *
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
 # Create your views here.
 
 def index(request):
-    #批量导入
+    # 批量导入
     # Booklist=[]
     # for i in range(100):
     #     Booklist.append(Book(title='book'+str(i),price=30+i*i))
     # Book.objects.bulk_create(Booklist)
+    try:
+        book_list = Book.objects.all()
+        p = Paginator(book_list, 10)
+        page = request.GET.get("page")
+        book_list = p.page(page)
+    except PageNotAnInteger:
 
-    book_list=Book.objects.all()
+        book_list = p.page(1)
+    except EmptyPage:
 
-    return render(request,"index.html",locals())
+        book_list = p.page(p.num_pages)
+    return render(request, "index.html", locals())
