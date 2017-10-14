3c24a759997b07815102ce8bf57d09f3c627531d
ZQOFFICE0\wuxingpu <windfishing5@gmail.com>
2017-10-14 13:51:18 +0800

    1014

--- s17day20/app01/views.py
+++ s17day20/app01/views.py
@@ -1,17 +1,54 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse, redirect, redirect
 from django.views import View
+
+# class BaseView(View):
+#     def dispatch(self, request, *args, **kwargs):
+#         if request.session.get("username"):
+#             response = super(BaseView,self).dispatch(request, *args, **kwargs)
+#             return response
+#         else:
+#             redirect('/login.html')
 
 # Create your views here.
 class LoginView(View):
-    #执行父类的dispatch方法   父类的dispatch通过反射找get或post
-    def dispatch(self, request, *args, **kwargs):
-        print('before')
-        response=super(LoginView,self).dispatch(request, *args, **kwargs)
-        print('after')
-        return response
-    def get(self,request,*args,**kwargs):
-        return render(request,'login.html')
+    # 执行父类的dispatch方法   父类的dispatch通过反射找get或post
+    # def dispatch(self, request, *args, **kwargs):
+    #     print('before')
+    #     response=super(LoginView,self).dispatch(request, *args, **kwargs)
+    #     print('after')
+    #     return response
+    def get(self, request, *args, **kwargs):
+        return render(request, 'login.html')
 
-    def post(self,request,*args,**kwargs):
-        print('POST')
-        return HttpResponse('OK')
+    def post(self, request, *args, **kwargs):
+        user = request.POST.get('user')
+        pwd = request.POST.get('pwd')
+        if user == 'alex' and pwd == '123':
+            # sessionid=xxxxx写入客户端  ;  在数据库里存入{username:alex}
+            request.session['username'] = user
+            return redirect('/index.html')
+        return render(request, 'login.html', {'msg': 'login faild'})
+
+# class IndexView(BaseView):
+#
+#     def get(self, request, *args, **kwargs):
+#         print(request.session['username'])
+#         return HttpResponse(request.session['username'])
+
+#多继承
+class BaseView(object):
+    def dispatch(self, request, *args, **kwargs):
+        if request.session.get("username"):
+            response = super(BaseView, self).dispatch(request, *args, **kwargs)
+            return response
+        else:
+            redirect('/login.html')
+
+
+class IndexView(BaseView,View):
+    def get(self, request, *args, **kwargs):
+        print(request.session['username'])
+        return HttpResponse(request.session['username'])
+class SomeView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse(request.session['username'])
